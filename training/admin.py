26eab2a5eb32b3fa8cf5eb830e6ff319a630d8fb
patilacerda <patriciarlacerda@live.com>
2024-03-18 23:59:40 +0000

    Add summernote and Allauth

--- training/admin.py
+++ training/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Activity
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Activity)
+@admin.register(Activity)
+class ActivityAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'status')
+    search_fields = ['name']
+    list_filter = ('status',)
+    summernote_fields = ('description',)
