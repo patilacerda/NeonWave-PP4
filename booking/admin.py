4aab5b8a1c794c492f92b0bcfa7a193f538abc5c
patilacerda <patriciarlacerda@live.com>
2024-03-20 17:25:13 +0000

    Create model to populate the time available for activities

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Select
+from datetime import timedelta
+from .models import TimeAvailable
 
 # Register your models here.
+@admin.register(TimeAvailable)
+class TimeAvailableAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TimeField: {
+            'widget': Select(choices=[
+            ('08:00:00', '08:00 AM'),
+            ('09:00:00', '09:00 AM'),
+            ('10:00:00', '10:00 AM'),
+            ('15:00:00', '03:00 PM'),
+            ('16:00:00', '04:00 PM'),
+            ('17:00:00', '05:00 PM'),
+        ])},
+    }
+
+    list_display = ['activity', 'day', 'time']
+    list_filter = ['activity', 'day', 'time']
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from training.models import Activity
 
 # Create your models here.
+class TimeAvailable(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    day = models.DateField()
+    time = models.TimeField()
+
+    class Meta:
+        unique_together = ('activity', 'day', 'time')
+        verbose_name = "Time Available"
+        verbose_name_plural = "Times Available"
+
+    def __str__(self):
+        return f"{self.activity.name} - {self.day} at {self.time.strftime('%H:%M')}"
