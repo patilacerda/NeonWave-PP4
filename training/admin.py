7911aad575a57f02c0bf10362a49db2b7cd9955d
patilacerda <patriciarlacerda@live.com>
2024-03-22 06:51:38 +0000

    Update training model to enable serving of image files

--- training/admin.py
+++ training/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.text import slugify
 from .models import Activity
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -10,3 +11,13 @@ class ActivityAdmin(SummernoteModelAdmin):
     search_fields = ['name']
     list_filter = ('status',)
     summernote_fields = ('description',)
+
+    def save_model(self, request, obj, form, change):
+        """
+        Prepopulate the slug based on the name field.
+        """
+        if not obj.slug:
+            obj.slug = slugify(obj.name)
+        super().save_model(request, obj, form, change)
+
+    prepopulated_fields = {'slug': ('name',)}
--- training/models.py
+++ training/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Available"))
 
@@ -9,10 +11,17 @@ class Activity(models.Model):
     Stores a single Activity available entry
     """
     name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     duration = models.DurationField()
     status = models.IntegerField(choices=STATUS, default=0)
     max_capacity = models.PositiveIntegerField(default=10)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
