4e7456bb058c80287478024f89cdb4d722dc52d7
patilacerda <patriciarlacerda@live.com>
2024-03-29 18:37:36 +0000

    Add edit and delete buttons to comments

--- training/urls.py
+++ training/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ActivityList.as_view(), name='home'),
+    path('comments/<slug>', views.activity_comment, name='activity_comments'),
+    path('comments/<slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
--- training/views.py
+++ training/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -15,7 +15,6 @@ def activity_comment(request, slug):
     activity = get_object_or_404(Activity, slug=slug, status=1)
     comments = activity.comments.all()
     comment_count = activity.comments.filter(approved=True).count()
-    comment_form = CommentForm()
     
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
@@ -28,10 +27,10 @@ def activity_comment(request, slug):
             request, messages.SUCCESS,
             'Comment submitted and awaiting approval'
             )
-
+    comment_form = CommentForm()
     return render(
     request,
-    "activity_list.html",
+    "training/activity_detail.html",
     {
         "object_list": [activity],
         "comments": comments,
@@ -39,3 +38,42 @@ def activity_comment(request, slug):
         "comment_form": comment_form,
     },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+
+    """
+    if request.method == "POST":
+
+        queryset = activity.objects.filter(status=1)
+        activity = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.user == request.user:
+            comment = comment_form.save(commit=False)
+            comment.activity = activity
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('activity_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+
+    """
+    queryset = Post.objects.filter(status=1)
+    activity = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.user == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('activity_detail', args=[slug]))
